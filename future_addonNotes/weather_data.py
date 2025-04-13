9a67c58ddf237cf68e9c8007d379ac6dffe51b0d
Daniel Schubert <daschub496@gmail.com>
2025-04-13 06:31:07 +0200

    remove hourly class call

--- future_addonNotes/weather_data.py
+++ future_addonNotes/weather_data.py
@@ -7,10 +7,10 @@
 
 import asyncio
 import python_weather
-from typing import Tuple, Optional
+from typing import Tuple
 from datetime import datetime
 
-async def get_temperature(location) -> Tuple[int, datetime, Optional[int]]:
+async def get_temperature(location) -> Tuple[int, datetime]:
     """Fetch the current temperature for a given location.
     @class Client: creates a session with this @url='https://wttr.in/Berlin?format=j1'
     additionally client.py imports the library's class Forecast to get the weather data
@@ -20,22 +20,16 @@ async def get_temperature(location) -> Tuple[int, datetime, Optional[int]]:
             # Fetch the weather data for the specified location
             # The location can be a city name, coordinates, or a postal code
             weather = await client.get(location)
-            # Initialize default values for hourly_temperature and hourly_time
-            hourly_temperature = None
-            # Assuming you want to access the first daily forecast's temperature
-            if weather.daily_forecasts[0].hourly_forecasts:
-                hourly_forecasts = weather.daily_forecasts[0].hourly_forecasts[0]
-                hourly_temperature = hourly_forecasts.temperature
-            return weather.temperature, weather.datetime, hourly_temperature
+            # Extract the temperature and datetime from the weather data
+            return weather.temperature, weather.datetime
     except Exception as e:
         print(f"An error occurred: {e}")
         print("Please check your internet connection or the location name.")
         raise
 
 if __name__ == "__main__":
-    # Get the temperature for Berlin-Bezirk Neukölln
+    # Get the temperature for Berlin
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    temperature, local_time, hourly_temp= asyncio.run(get_temperature("Berlin-Bezirk Neukölln"))
-    print(f"The temperature in Berlin-Bezirk Neukölln is {temperature}°C")
+    temperature, local_time = asyncio.run(get_temperature("Berlin"))
+    print(f"The temperature in Berlin is {temperature}°C")
     print(f"Latest Local time: {local_time}")
-    print(f"Last Hourly temperature: {hourly_temp}°C")
